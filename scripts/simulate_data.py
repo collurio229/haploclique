0794c09b8f3c92b3d29716680abef5b4b4d223c9
Bernhard Lang <bernhard.lang@gmx.de>
2015-03-25 10:22:25 +0100

    Outsourced Sequence class

--- scripts/simulate_data.py
+++ scripts/simulate_data.py
@@ -10,99 +10,8 @@ import sys
 import os
 
 from progressdone import *
+from Sequence import *
 
-class Sequence:
-
-    def __init__(self, identifier, description, bases=['A', 'G', 'C', 'T']):
-        self.identifier = identifier
-        self.description = description
-        self.sequence = []
-        self.bases = bases
-
-        self.base_freqs = dict()
-        for b in self.bases:
-            self.base_freqs[b] = 0.25
-
-    def replicate(self):
-        new_seq = Sequence(self.identifier, self.description)
-        new_seq.add_sequence(''.join(self.sequence))
-
-        return new_seq
-
-    def add_sequence(self, seq):
-        old_len = len(self.sequence)
-        self.sequence.extend(list(seq))
-
-        length = len(self.sequence)
-
-        new_len = len(seq)
-
-        new_freq = dict()
-        for key in self.base_freqs.keys():
-            new_freq[key] = 0
-
-        for c in seq:
-            new_freq[c] += 1
-            
-        for (base, freq) in new_freq.items():
-            self.base_freqs[base] = (old_len * self.base_freqs[base] + freq) / length
-
-    def sample_base(self):
-        r = random.random()
-        ct = 0
-
-        for (base, freq) in self.base_freqs.items():
-            ct += freq
-            if r < ct:
-                return base
-
-    def create_snp(self, dist_fn=lambda self: self.sample_base(), pos=-1, base='N'):
-        if pos < 0 or pos > len(self.sequence):
-            pos = random.randint(0, len(self.sequence)-1)
-
-        if base == 'N':
-            base = dist_fn(self)
-
-        self.sequence[pos] = base
-
-    def create_insertion(self, mean, std_deviation, pos=-1):
-        length = int(random.gauss(mean, std_deviation))
-
-        if pos < 1 or pos > len(self.sequence):
-            pos = random.randint(0, len(self.sequence)-1)
-
-        insert = []
-
-        for i in range(0, length):
-
-            insert.append(self.sample_base())
-
-        self.sequence[pos:pos] = insert
-
-    def create_deletion(self, mean, std_deviation, pos=-1):
-        length = int(random.gauss(mean, std_deviation))
-
-        if pos < 1 or pos > len(self.sequence)-length:
-            pos = random.randint(0, len(self.sequence)-length-1)
-
-        self.sequence[pos:pos+length] = []
-
-    def writeFASTA(self, fileobj):
-        fileobj.write(bytes('>' + self.identifier + '|' + self.description + '\n', 'UTF-8'))
-        ct = 0
-        for base in self.sequence:
-            fileobj.write(bytes(base, 'UTF-8'))
-            ct += 1
-            if ct == 70:
-                ct = 0
-                fileobj.write(b'\n')
-
-class ParsingError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return repr(self.message)
 
 class ExternalError(Exception):
     def __init__(self, message):
@@ -110,24 +19,6 @@ class ExternalError(Exception):
 
     def __str__(self):
         return repr(self.message)
-
-
-def readFASTA(filename):
-    with open(filename, 'r') as f:
-        header = f.readline()
-
-        m = re.search('>(?P<identifier>\w*)\|(?P<description>.*)', header)
-        if not m:
-            raise ParsingError('FASTA header was not valid')
-
-        seq = Sequence(m.group('identifier'), m.group('description'))
-
-        for line in f:
-            seq.add_sequence(line.rstrip())
-
-    return seq
-
-
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Generate haplotypes from a reference genome and create simulated illumina reads with SimSeq')
