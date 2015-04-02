0ecd84112957934ec26f0ec1feade1cda7b0e5f7
Bernhard Lang <bernhard.lang@gmx.de>
2015-04-02 15:51:30 +0200

    Created haploclique assembly python script

---
+++ scripts/haploclique.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Run haploclique on some files
+
+Usage:
+  haploclique.py [options] [--] <reference> <input>
+  haploclique.py -h | --help
+
+  <reference>   Reference genome in fasta format
+  <input>       Read alignment in bam format
+
+Options:
+  
+"""
+def initialize():
+    pass
+
+def main(argv):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
